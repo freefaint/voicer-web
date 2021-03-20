3ecd934c078711ec75e32b8c2a25a1d9415e15a3
Андрей Терещенко <andrejteresenko@MacBook-Air-Andrej.local>
2021-03-21 01:46:36 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,66 @@
-import React from 'react';
+/* eslint-disable import/no-webpack-loader-syntax */
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import products from './prods.json';
+
+const check = (results: string[]) => {
+  
+}
+
 function App() {
+  // const [ text, setText ] = useState<string | undefined>();
+  const [ name, setName ] = useState<string>('');
+  const [ img, setImg ] = useState<string>(logo);
+  const [ result, setResult ] = useState<string>('');
+
+  useEffect(() => {
+    // @ts-ignore
+    const voice = new webkitSpeechRecognition();
+
+    voice.lang = 'ru-RU';
+    voice.continious = true;
+    voice.interimResults = true;
+
+    voice.onerror = (err: any) => {
+      console.log('error', err);
+    }
+
+    voice.onresult = (e: any) => {
+      const results = Object.keys(e.results[0]).filter(i => parseInt(i, 10).toString() === i).map(i => e.results[0][i].transcript);
+      
+      // check(results);
+      setResult(results[0]);
+      console.log(results);
+    }
+
+    voice.onend = () => {
+      console.log('end');
+
+      setTimeout(() => voice.start(), 100);
+    }
+
+    voice.start();
+  }, []);
+
+  useEffect(() => {
+    const prod = products.find(i => i.names.find(j => result.indexOf(j) !== -1));
+
+    if (prod) {
+      setName(prod.name);
+      setImg(prod.img);
+    }
+
+  }, [ result ])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={img} className="App-logo" alt="logo" />
+        <pre>
+          {name}
+        </pre>
       </header>
     </div>
   );
