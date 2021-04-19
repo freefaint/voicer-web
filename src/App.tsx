2a38f270034da69e93bc46b3a8f1cf895de87e8f
Андрей Терещенко <andrejteresenko@inca-9801-0124.sigma.sbrf.ru>
2021-04-19 17:23:36 +0300

    design

--- src/App.tsx
+++ src/App.tsx
@@ -5,40 +5,64 @@ import { useSpeech } from './hooks/useSpeech';
 
 import products from './products.json';
 
-// interface Product {
-//   name: string;
-//   names: string[];
-//   img: string;
-// }
+interface Product {
+  id: string;
+  name: string;
+  cost: string;
+  weight: string;
+  category: string;
+  age: string;
+  description: string;
+  textcolor: string;
+  names: string[];
+  img: string;
+}
 
 function App() {
-  const [ name, setName ] = useState<string>('');
-  const [ img, setImg ] = useState<string>();
   const [ showText, setShowText ] = useState(true);
 
   const speech = useSpeech();
-  const db = useDB(products)
+  const db = useDB<Product>(products);
 
-  const data = products || db;
+  const data: Product[] = products || db;
+
+  const [ product, setProduct ] = useState<Product>(products[0]);
 
   useEffect(() => {
     console.log(speech);
-    const prod = data.find(i => i.names.find(j => speech?.results.find(k => k.indexOf(j) !== -1)));
+    const target = data.find(i => i.names.find(j => speech?.results.find(k => k.indexOf(j) !== -1)));
 
-    if (prod) {
-      setName(prod.name);
-      setImg(prod.img);
+    if (target) {
+      setProduct(target);
     }
     
-    if (speech?.results.find(i => i.indexOf('показать текст') !== -1)) {
+    if (speech?.final && speech?.results.find(i => i.indexOf('показать название') !== -1)) {
       setShowText(showText => !showText);
     }
 
   }, [ speech, db, setShowText, data ])
 
   return  (
-    <div className="App" style={{ backgroundImage: `url(${img})` }}>
-      {showText ? name : ''}
+    <div className="Block Column JustifySpace App" style={{ backgroundImage: `url(${product.img})` }}>
+      {showText && (
+        <>
+          <div className="Block Column">
+            <div className="Block JustifySpace">
+              <div className="Block Name">{product.name}</div>
+              <div className="Block Eighteen">18+</div>
+            </div>
+            
+            <div className="Block JustifyEnd">
+              <div className="Block Price">{product.cost}</div>
+            </div>
+          </div>
+
+          <div className="Block JustifySpace AlignCenter">
+            <div className="Block Desc">{product.description}</div>
+            <div className="Block Weight">{product.weight}</div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/hooks/useSpeech.ts
+++ src/hooks/useSpeech.ts
@@ -12,18 +12,17 @@ export const useSpeech = () => {
     voice.interimResults = true;
 
     voice.onerror = (err: any) => {
-      console.log('error', err);
+      // console.log('error', err);
     }
 
     voice.onresult = (e: any) => {
       const results = Object.keys(e.results[0]).filter(i => parseInt(i, 10).toString() === i).map(i => e.results[0][i].transcript);
       
       setResult({ results, final: e.results[0].isFinal });
-      console.log(results);
     }
 
     voice.onend = () => {
-      console.log('end');
+      // console.log('end');
 
       setTimeout(() => voice.start(), 100);
     }
