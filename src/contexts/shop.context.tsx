b03e0fa396c6a00947a8c1d25d159a44f9af6710
Андрей Терещенко <andrejteresenko@MacBook-Air-Andrej.local>
2021-05-16 21:36:24 +0300

    fixed search logic

--- src/contexts/shop.context.tsx
+++ src/contexts/shop.context.tsx
@@ -12,7 +12,7 @@ import { CartContext } from "./cart.context";
 export const ShopContext = createContext<Shop | undefined>(undefined);
 
 export const ShopProvider = ({ children, products }: PropsWithChildren<{ products: Product[] }>) => {
-  const demoTimer = useTimeout(60);
+  const demoTimer = useTimeout(180);
 
   const [ currentId, setCurrentId ] = useState<string>();
   const [ count, setCount ] = useState(1);
@@ -39,8 +39,32 @@ export const ShopProvider = ({ children, products }: PropsWithChildren<{ product
   }, [ demoTimer.seconds ]);
 
   useEffect(() => {
-    console.log(speech);
-    const target = products.find(i => speech?.results.find(k => k.toLowerCase().indexOf(i.name.toLowerCase()) !== -1) || i.names.find(j => speech?.results.find(k => k.toLowerCase().indexOf(j.toLowerCase()) !== -1)));
+    let target;
+    let variants = products.map(i => ({
+      ...i,
+      mainResults: speech?.results.map(k => ({ speech: k, ok: k.toLowerCase().indexOf(i.name.replace(/[\(\)]/, '').toLowerCase()) !== -1 }) ),
+      namesResults: i.names.map(j => ({ name: j, results: speech?.results.map(k => ({ speech: k, ok: k.toLowerCase().indexOf(j.toLowerCase()) !== -1 }) ) }) )
+    }) ).filter(i => 
+      i.mainResults?.find(j => j.ok) ||
+      i.namesResults?.find(j => j.results?.find(k => k.ok)));
+    
+    console.log('VARIANTS', variants);
+    
+    if (variants.find(i => i.mainResults?.find(j => j.ok))) {
+      variants = variants.filter(i => i.mainResults?.find(j => j.ok)).sort((a, b) => a.name.replace(/[\(\)]/, '').length > b.name.replace(/[\(\)]/, '').length ? -1 : 1);
+    } else {
+      variants = variants.sort((a, b) => {
+        const first = Math.max(...a.namesResults.filter(i => i.results?.find(j => j.ok)).map(i => i.name.length));
+        const second = Math.max(...b.namesResults.filter(i => i.results?.find(j => j.ok)).map(i => i.name.length));
+
+        return first > second ? -1 : first < second ? 1 : 0;
+      });
+    }
+    
+    console.log('SORTED VARIANTS', variants);
+    console.log('FINAL VARIANT', variants[0]);
+
+    target = variants[0];
 
     if (target && currentId !== target.id) {
       setCurrentId(target.id);
