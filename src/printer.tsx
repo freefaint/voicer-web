add8714892726f431ad430c1d14fd1985662d4fd
Андрей Терещенко <andrejteresenko@192.168.1.9>
2021-06-06 00:17:17 +0300

    test printer

--- src/printer.tsx
+++ src/printer.tsx
@@ -1,6 +1,8 @@
 import escpos from 'escpos';
 // install escpos-usb adapter module manually
 import USB from 'escpos-usb';
+import EscPosEncoder from 'esc-pos-encoder';
+
 import * as OrderService from './rest/order';
 import { Order } from './types/order';
 // Select the adapter based on your printer type
@@ -8,8 +10,8 @@ const device  = new USB();
 // const device  = new escpos.Network('localhost');
 // const device  = new escpos.Serial('/dev/usb/lp0');
 
-// const options = { encoding: "GB18030" /* default */ }
-const options = { encoding: "windows1251" /* default */ }
+const options = { encoding: "GB18030" /* default */ }
+// const options = { encoding: "windows1251" /* default */ }
 // encoding is optional
 
 const printer = new escpos.Printer(device, options);
@@ -24,10 +26,10 @@ const getLatest = () => {
 
     device.open(function(error: any) {
       printer
-      .font('A')
-      .align('CT')
-      .style('BU')
-      .size(1, 1)
+      // .font('A')
+      // .align('CT')
+      // .style('BU')
+      // .size(1, 1)
       .text('Заказ № ' + item.orderNumber + (item.export ? ' с собой' : ''))
       .text('Дата заказа ' + new Date(item.date).toLocaleString())
       .text('')
@@ -35,47 +37,52 @@ const getLatest = () => {
       .text('')
       .text('Итого: ' + item.total + ' руб.')
       .text('')
-      .text('Терминалы самообслуживания с голосовым модулем voice-shop.ru тел. +7 (929) 632 5522')
-      // .barcode('1234567', 'EAN8')
-      // .table(["One", "Two", "Three"])
-      // .tableCustom([
-      //   { text:"Left", align:"LEFT", width:0.33 },
-      //   { text:"Center", align:"CENTER", width:0.33},
-      //   { text:"Right", align:"RIGHT", width:0.33 }
-      // ])
-      .qrimage('https://freefaint.ru', function(err){
-        printer.cut();
-        printer.close();
-      });
+      // .text('Терминалы самообслуживания с голосовым модулем voice-shop.ru тел. +7 (929) 632 5522')
+      // .qrimage('https://freefaint.ru', function(err){
+      //   printer.cut();
+      //   printer.close();
+      // });
     });
 
     latest = item;
   });
 }
 
-getLatest();
+// getLatest();
 
-setInterval(getLatest, 5000);
+// setInterval(getLatest, 5000);
 
 
+
+let encoder = new EscPosEncoder();
+
+let result = encoder
+    .initialize()
+    .text('The quick brown fox jumps over the lazy dog')
+    .newline()
+    .text('Привет')
+    // .qrcode('https://nielsleenheer.com')
+    .encode();
 
 device.open(function(error: any) {
   printer
-  .font('A')
-  .align('CT')
-  .style('BU')
-  .size(1, 1)
+  // .font('A')
+  // .align('CT')
+  // .style('BU')
+  // .size(1, 1)
   .text('The quick brown fox jumps over the lazy dog')
-  .text('hello')
-  .barcode('1234567', 'EAN8')
-  .table(["One", "Two", "Three"])
+  .print(result)
+  .cut()
+  .close()
+  // .barcode('1234567', 'EAN8')
+  // .table(["One", "Two", "Three"])
   // .tableCustom([
   //   { text:"Left", align:"LEFT", width:0.33 },
   //   { text:"Center", align:"CENTER", width:0.33},
   //   { text:"Right", align:"RIGHT", width:0.33 }
   // ])
-  .qrimage('https://github.com/song940/node-escpos', function(err){
-    printer.cut();
-    printer.close();
-  });
+  // .qrimage('https://github.com/song940/node-escpos', function(err){
+  //   printer.cut();
+  //   printer.close();
+  // });
 });
