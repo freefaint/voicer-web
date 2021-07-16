809fa1caa5824f338497b628449a595c1f36a551
Андрей Терещенко <andrejteresenko@MacBook-Air-Andrej.local>
2021-07-16 21:04:04 +0300

    fixed printer connect method

--- src/printer.tsx
+++ src/printer.tsx
@@ -1,12 +1,12 @@
 import escpos from 'escpos';
 // install escpos-usb adapter module manually
-import USB from 'escpos-usb';
+// import USB from 'escpos-usb';
 
 import * as OrderService from './rest/order';
 import { Order } from './types/order';
 // Select the adapter based on your printer type
-const device  = new USB();
-// const device  = new escpos.Network('localhost');
+// const device  = new USB();
+const device = new escpos.Network('localhost');
 // const device  = new escpos.Serial('/dev/usb/lp0');
 
 const options = { encoding: "cp866" /* default */ }
